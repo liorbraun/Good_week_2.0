16661ee4ffce6632f4e83be64c4e1ffc0474b7fc
liorbraun <54171574+liorbraun@users.noreply.github.com>
2025-02-09 23:26:37 +0200

    test8

--- src/pages/Homoscope.jsx
+++ src/pages/Homoscope.jsx
@@ -1,22 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DBContext } from "../context/SheetParsing";
 import "../style/Homoscope.css";
 
 const Homoscope = () => {
-
+    const [ displayContent, setDisplayContent ] = useState(null);
     const {homoscope} = useContext(DBContext)       
-    const onClickhabdler = () => {} 
+    const onClickHandler = (content) => { 
+        return () => {
+            setDisplayContent(content);
+        }
+     } 
     return (
         <div className="homoscope">
             <div className="homoscopeWrapper">
                 {homoscope && homoscope.map((item, index) => {
                     return (
-                        <div key={index} className="homoscopeItem" onClick= { onClickhabdler } >
+                        <div key={index} className="homoscopeItem" onClick= { onClickHandler(item.content) } >
                             <p className="homoscopeTitle">{item.horoscope}</p>
-                            { <p>{item.content}</p> }
+                            
                         </div>
-                    )
+                    )    
                 })}
+                 { <p>{displayContent}</p> }
             </div>
         </div>
     );

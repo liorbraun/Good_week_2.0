a0d329e878d9febf8166550c74b29abcfddddbff
Roei <r.balisha@gmail.com>
2025-02-10 19:04:23 +0200

    Completed Homoscope page

--- src/pages/Homoscope.jsx
+++ src/pages/Homoscope.jsx
@@ -3,25 +3,31 @@ import { DBContext } from "../context/SheetParsing";
 import "../style/Homoscope.css";
 
 const Homoscope = () => {
-    const [ displayContent, setDisplayContent ] = useState(null);
+    const [ displayContent, setDisplayContent ] = useState();
     const {homoscope} = useContext(DBContext)       
+    
     const onClickHandler = (content) => { 
         return () => {
             setDisplayContent(content);
+            console.log(homoscope[0].content);
         }
-     } 
+    }
+    
     return (
         <div className="homoscope">
             <div className="homoscopeWrapper">
-                {homoscope && homoscope.map((item, index) => {
-                    return (
-                        <div key={index} className="homoscopeItem" onClick= { onClickHandler(item.content) } >
-                            <p className="homoscopeTitle">{item.horoscope}</p>
-                            
-                        </div>
-                    )    
-                })}
-                 { <p>{displayContent}</p> }
+                <div className="homoscopeitems">
+                    {homoscope && homoscope.map((item, index) => {
+                        return (
+                            <div key={index} className={`homoscopeItem ${(index === displayContent) ? "selected" : ""}`} onClick= { onClickHandler(index) } >
+                                <p className="homoscopeTitle">{item.horoscope}</p>
+                            </div>
+                        )    
+                    })}
+                </div>
+                <div className="homoscopecontent">
+                 { displayContent !== undefined && <p>{homoscope[displayContent].content}</p> }
+                </div>
             </div>
         </div>
     );

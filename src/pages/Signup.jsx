faaa070e300e688b5375c5a5b2c73df60d4d14c7
Roei <r.balisha@gmail.com>
2025-02-14 11:29:51 +0200

    fix form in mobile

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,7 +6,6 @@ const Signup = () => {
             <div className="formWrapper">
                 <iframe 
                     src="https://docs.google.com/forms/d/e/1FAIpQLSdVLgCgiYVLdWsiRpyfyPYh-F169Jq0w9IyipNXMttapHqdAA/viewform?embedded=true"
-                    width="640"
                     height="750"
                     frameborder="0"
                     marginheight="0" 
